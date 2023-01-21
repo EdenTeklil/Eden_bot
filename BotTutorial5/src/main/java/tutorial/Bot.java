3b4c224a72ce6fd90d538ed79c7ded0d09dbb959
Eden <edustargal@gmail.com>
2023-01-21 12:44:31 +0300

    myself commit

---
+++ BotTutorial5/src/main/java/tutorial/Bot.java
@@ -0,0 +1,43 @@
+package tutorial;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class Bot extends TelegramLongPollingBot {
+
+    @Override
+    public String getBotUsername() {
+        return "edenedenn_bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "5424134899:AAF1PIRx4JPmaa7Oys6peIBqUh2fuo2UVRg";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        var msg = update.getMessage();
+        var user = msg.getFrom();
+        var id = user.getId();
+
+        sendText(id, msg.getText());
+    }
+    public void sendText(Long who, String what){
+        SendMessage sm = SendMessage.builder()
+                .chatId(who.toString())
+                .text(what).build();
+        try {
+            execute(sm);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+
+
+}
